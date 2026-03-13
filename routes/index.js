7751fd231af33f91a56391b397ce241e1846a400
AlliedProduct <nojusnoah@gmail.com>
2026-03-13 20:31:05 +0000

    health endpoint

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: "Simple Application" });
 });
 
+router.get('/health', function(req, res) {
+  res.status(200).json({ status: 'ok' });
+});
 
 module.exports = router;
